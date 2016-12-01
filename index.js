dfca327a395e5bfc67f9c2d6e8fd0f1207b1d2d4
Vinayak Malladad <vmalladad@cpapi-sandbox-ukapi-m03.ihost.aol.com>
2016-12-01 00:45:51 -0500

    dd

--- index.js
+++ index.js
@@ -81,40 +81,10 @@ function sendTextMessage(sender, text) {
 }
 
 function sendGenericMessage(sender) {
-	let messageData = {
-		"attachment": {
-			"type": "template",
-			"payload": {
-				"template_type": "generic",
-				"elements": [{
-					"title": "First card",
-					"subtitle": "Element #1 of an hscroll",
-					"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-					"buttons": [{
-						"type": "web_url",
-						"url": "https://www.messenger.com",
-						"title": "web url"
-					}, {
-						"type": "postback",
-						"title": "Postback",
-						"payload": "Payload for first element in a generic bubble",
-					}],
-				}, {
-					"title": "Second card",
-					"subtitle": "Element #2 of an hscroll",
-					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-					"buttons": [{
-						"type": "postback",
-						"title": "Postback",
-						"payload": "Payload for second element in a generic bubble",
-					}],
-				}]
-			}
-		}
-	}
+	
 	 
 	
-	let messageData1= {
+	let messageData= {
 			"attachment":{
 			      "type":"template",
 			      "payload":{
